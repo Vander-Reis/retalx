85570fb9d1b6a3d3657ceb0d655147bdd541a4de
Vander-Reis <vanderreis2017@outlook.com>
2022-12-26 15:12:37 -0300

    Criando migration de especificação

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,15 +1,18 @@
-import { Entity, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
-@Entity("specification")
+@Entity("specifications")
 class Specification {
   @PrimaryColumn()
   id?: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
