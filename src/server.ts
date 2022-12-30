b55be8fb62a90d37c360b5b67ca910a6782e4f9c
Vander-Reis <vanderreis2017@outlook.com>
2022-12-30 16:33:20 -0300

    Imports da aplicação

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,13 @@ import "express-async-errors";
 import { SwaggerTheme } from "swagger-themes";
 import swaggerUi from "swagger-ui-express";
 
-import { AppError } from "./errors/AppError";
+import "@shared/container";
+import { AppError } from "@errors/AppError";
+
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
 import "./database";
-import "./shared/container";
 
 const app = express();
 
