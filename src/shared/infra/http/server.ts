0bc34d5ea20b9512ff8144ff5015f29aa3825e85
Vander-Reis <vanderreis2017@outlook.com>
2022-12-30 17:43:00 -0300

    Refatorando a aplicação

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,13 +4,13 @@ import "express-async-errors";
 import { SwaggerTheme } from "swagger-themes";
 import swaggerUi from "swagger-ui-express";
 
+import "@shared/infra/typeorm";
+
 import "@shared/container";
-import { AppError } from "@errors/AppError";
+import { AppError } from "@shared/errors/AppError";
 
+import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
-import swaggerFile from "./swagger.json";
-
-import "./database";
 
 const app = express();
 
@@ -24,6 +24,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile, optionsV1));
 app.use(router);
 
 app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
