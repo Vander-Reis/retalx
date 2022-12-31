7de74412a9e6d64b2395de5bd8a1505f261957ac
Vander-Reis <vanderreis2017@outlook.com>
2022-12-31 11:29:29 -0300

    row query

--- src/modules/cars/infra/typeorm/repositories/CategoriesRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CategoriesRepository.ts
@@ -30,7 +30,7 @@ class CategoriesRepository implements ICategoriesRepository {
   }
 
   async list(): Promise<Category[]> {
-    const categories = await this.repository.find();
+    const categories = await this.repository.query(`SELECT * FROM categories`);
     return categories;
   }
 
