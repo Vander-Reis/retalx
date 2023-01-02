14b0a62953035e556946093ccfb5eb17251a80a5
Vander-Reis <vanderreis2017@outlook.com>
2023-01-02 18:24:06 -0300

    Continuando o caso de uso de carros

--- src/modules/cars/useCases/CreateCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/CreateCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateCarUseCase } from "./CreateCarUseCase";
 
@@ -12,7 +13,7 @@ describe("Create car", () => {
   });
 
   it("should be able to create a new car", async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: "Name Car",
       description: "Description car",
       daily_rate: 100,
@@ -21,5 +22,44 @@ describe("Create car", () => {
       brand: "brand",
       category_id: "category",
     });
+
+    expect(car).toHaveProperty("id");
+  });
+
+  it("should not be to create a car with exists license plante", () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "Car1",
+        description: "Description car",
+        daily_rate: 100,
+        license_plate: "abc-12340",
+        fine_amount: 60,
+        brand: "brand",
+        category_id: "category",
+      });
+
+      await createCarUseCase.execute({
+        name: "Car2",
+        description: "Description car",
+        daily_rate: 100,
+        license_plate: "abc-12340",
+        fine_amount: 60,
+        brand: "brand",
+        category_id: "category",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should be able to create a car with avaliable true by default ", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Car Avaliable",
+      description: "Description car",
+      daily_rate: 100,
+      license_plate: "abcd-12340",
+      fine_amount: 60,
+      brand: "brand",
+      category_id: "category",
+    });
+    expect(car.available).toBe(true);
   });
 });
