6ec27bf9fd22e26da372aecf93cd8505590b08c3
Vander-Reis <vanderreis2017@outlook.com>
2023-01-05 16:08:41 -0300

    Configurando supertest

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,42 @@
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import { SwaggerTheme } from "swagger-themes";
+import swaggerUi from "swagger-ui-express";
+
+import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
+
+import "@shared/container";
+
+import swaggerFile from "../../../swagger.json";
+import { router } from "./routes";
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+
+const theme = new SwaggerTheme("v3");
+const optionsV1 = theme.getDefaultConfig("dark");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile, optionsV1));
+
+app.use(router);
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
+export { app };
