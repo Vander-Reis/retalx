b17bc22acabcd1874f51888ff3411f4038358892
Vander-Reis <vanderreis2017@outlook.com>
2023-01-10 09:51:51 -0300

    Controller de devolução de carro

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -1,4 +1,4 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
@@ -11,18 +11,19 @@ interface IRequest {
   user_id: string;
 }
 
+@injectable()
 class DevolutionRentalUseCase {
   constructor(
     @inject("RentalsRepository")
     private rentalsRepository: IRentalsRepository,
     @inject("CarsRepository")
     private carsRepository: ICarsRepository,
-    @inject("DayJsDateProvider")
+    @inject("DayjsDateProvider")
     private dateProvider: IDateProvider
   ) {}
   async execute({ id, user_id }: IRequest): Promise<Rental> {
     const rental = await this.rentalsRepository.findById(id);
-    const car = await this.carsRepository.findById(id);
+    const car = await this.carsRepository.findById(rental.car_id);
     const minimum_dayle = 1;
 
     if (!rental) {
