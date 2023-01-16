26c578b2c7a98ddee4f3c0231be0fe1ce5df9246
Vander-Reis <vanderreis2017@outlook.com>
2023-01-16 16:58:25 -0300

    Criando provider de e-mail

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,16 @@ import { container } from "tsyringe";
 
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
+
+import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 
 container.registerSingleton<IDateProvider>(
   "DayjsDateProvider",
   DayjsDateProvider
+);
+
+container.registerInstance<IMailProvider>(
+  "EtherealMailProvider",
+  new EtherealMailProvider()
 );
